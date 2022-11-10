770198767ef417150b75b0912fbe064321c1ae53
Anietie Brownson <anni.brownson@gmail.com>
2022-11-10 17:50:06 +0100

    did some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import T10 from './assets/Ticket10k.jpg'
+import Flipside from './assets/Flipside.png'
+import Home from './assets/Home.png'
+import T10 from './assets/Ticket.jpg'
 import T15 from './assets/Ticket150k.jpg'
 import "./App.css";
 
@@ -9,14 +11,12 @@ function App() {
 
   return (
     <>
-      <img src='https://homeandawayfoods.com/Frontend/assets/img/logo/logo2.2.png' className='logo'/>
-      <div className='wrapper'>
+      <div>
         <div className='card'>
-          <a href='https://paystack.com/pay/only-me' target="_blank"> <img src={T10} alt='Thursday Throwback concert ticket for singles' /> </a>
-        </div>
-
-        <div className='card'>
-        <a href='https://paystack.com/pay/me-n-my-guys' target="_blank"> <img src={T15} alt='Thursday Throwback concert ticket for me and my guys' /> </a>
+          <img src={T10} alt='' />
+          <img src={T15} alt='' />
+          <img src={T10} alt='' />
+          <img src={T15} alt='' />
         </div>
       </div>
     </>
