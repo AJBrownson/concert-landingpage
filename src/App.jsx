85cd76269f53aa9f2f8cf101c0e42e9f75c6910c
Anietie Brownson <anni.brownson@gmail.com>
2022-11-11 16:05:46 +0100

    finishing touches

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Logo from './assets/Logo.png'
 import T10 from './assets/Ticket.jpg'
 import T150 from './assets/Ticket150k.jpg'
 import T50 from './assets/Ticket50k.png'
-import T300 from './assets/Ticket300k.png'
+import T300 from './assets/Ticket300k.jpg'
 import "./App.css";
 
 
@@ -20,13 +20,13 @@ function App() {
           <a href='https://paystack.com/pay/only-me' target='_blank'>
             <img src={T10} alt='' />
           </a>
-          <a href='#' target='_blank'>
+          <a href='https://paystack.com/pay/me-n-my-girl' target='_blank'>
             <img src={T50} alt='' />
           </a>
           <a href='https://paystack.com/pay/me-n-my-guys' target='_blank'>
             <img src={T150} alt='' />
           </a>
-          <a href='#' target='_blank'>
+          <a href='https://paystack.com/pay/we-plenty' target='_blank'>
             <img src={T300} alt='' />
           </a>
         </div>
